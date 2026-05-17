4c0cd42237acdac88f200458f6721bccfe865350
devvk <info@sekretov.net>
2026-05-17 17:16:18 +0200

    3. Доменная модель проекта "АвтоМаг" [#504897 #563913]

---
+++ db/scripts/002_ddl_create_auto_post_table.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+--changeset devvk:002_ddl_create_auto_post_table
+CREATE TABLE auto_post
+(
+    id           SERIAL PRIMARY KEY,
+    description  VARCHAR                       NOT NULL,
+    created      TIMESTAMP                     NOT NULL,
+    auto_user_id INT REFERENCES auto_user (id) NOT NULL,
+    UNIQUE (auto_user_id)
+);
+
+--rollback DROP TABLE auto_post;
