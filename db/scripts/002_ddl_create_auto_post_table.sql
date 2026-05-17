50294f476da3b92e7cceb165bba67a127bd5695a
devvk <info@sekretov.net>
2026-05-17 17:20:20 +0200

    3. Доменная модель проекта "АвтоМаг" [#504897 #563913]

--- db/scripts/002_ddl_create_auto_post_table.sql
+++ db/scripts/002_ddl_create_auto_post_table.sql
@@ -6,8 +6,7 @@ CREATE TABLE auto_post
     id           SERIAL PRIMARY KEY,
     description  VARCHAR                       NOT NULL,
     created      TIMESTAMP                     NOT NULL,
-    auto_user_id INT REFERENCES auto_user (id) NOT NULL,
-    UNIQUE (auto_user_id)
+    auto_user_id INT REFERENCES auto_user (id) NOT NULL
 );
 
 --rollback DROP TABLE auto_post;
